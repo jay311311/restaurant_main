7e06fd47ad5b8226dc722dc3d8a0a09b8216a27d
jay311311 <jay311311@gmail.com>
2020-09-28 08:07:30 +0900

    first_commit

--- sctript/script.js
+++ sctript/script.js
@@ -1,8 +1,19 @@
 const shopBox = document.querySelector(".shop")
 const foodBox = document.querySelector(".food")
+const login = document.querySelector("#show")
+const logout = document.querySelector("#hide")
+
 window.addEventListener('scroll', handleScroll );
+login.addEventListener("click", handleIn)
+logout.addEventListener("click", handleOut)
 
+function handleIn(){
+    document.querySelector(".form").style.display="block"
+}
 
+function handleOut(){
+    document.querySelector(".form").style.display="none"
+}
 function handleScroll(){
     if( window.pageYOffset >= 0){
         shopBox.classList.add("show")
