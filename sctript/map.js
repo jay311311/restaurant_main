193fab0816392b01f171532976ff810336970ae5
jay311311 <jay311311@gmail.com>
2020-09-29 19:28:12 +0900

    finally

--- sctript/map.js
+++ sctript/map.js
@@ -5,6 +5,7 @@ function initMap(){
         lat : 53.343825,
         lng : -6.266305 
     }
+
     const map = new google.maps.Map(
         mapContainer,
         {
@@ -13,6 +14,7 @@ function initMap(){
             zoom:18
         }
     )
+    
     const marker = new google.maps.Marker({
         position:myLatLong,
         map:map,
