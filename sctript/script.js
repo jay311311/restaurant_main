a8ccc04a3e5e450cdce046bf9727de9223ad6d7c
jay311311 <jay311311@gmail.com>
2020-10-07 14:15:26 +0900

    making loading icon

--- sctript/script.js
+++ sctript/script.js
@@ -2,10 +2,23 @@ const shopBox = document.querySelector(".shop")
 const foodBox = document.querySelector(".food")
 const login = document.querySelector("#show")
 const logout = document.querySelector("#hide")
+const wrap = document.querySelector("#wrap")
+const loading  = document.querySelector("#loading")
 
 window.addEventListener('scroll', handleScroll );
+window.addEventListener("DOMContentLoaded", handleLoad)
+window.addEventListener("load", handleLoading)
 login.addEventListener("click", handleIn)
 logout.addEventListener("click", handleOut)
+
+function handleLoading(){
+    wrap.style.display="block"
+    loading.style.display="none"
+}
+
+function handleLoad(){
+    wrap.style.display="none"
+}
 
 function handleIn(){
     document.querySelector(".form").style.display="block"
